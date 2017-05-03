69ab8a3c7cf7feadc28744888ff05a1f0548b131
ZickFz <zickfz@gmail.com>
2017-05-04 02:05:35 +0700

    day4

---
+++ day4/day4.js
@@ -0,0 +1,21 @@
+function Person(initialAge){
+    // Add some more code to run some checks on initialAge
+        if(initialAge < 0 ) {
+            initialAge = 0;
+            console.log("Age is not valid, setting age to 0.");
+       }
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+      if(initialAge < 13) {
+         console.log("You are young.");
+      }else if(initialAge >= 13 && initialAge < 18) {
+          console.log("You are a teenager.");
+      }else {
+          console.log("You are old.");
+      }
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+        initialAge += 1;
+   };
+}
